aa8379c6774529ad487f59a470ccee98d974da80
German Gambarte <germangambarte@gmail.com>
2026-08-14 00:15:26 -0300

    fix: replace all icons for drumstick image

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+      includeAssets: ['drumstick.svg'],
       manifest: {
         name: 'Fran Li',
         short_name: 'Fran Li',
